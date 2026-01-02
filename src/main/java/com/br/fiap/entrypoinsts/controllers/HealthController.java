d7a05a52a0552348979cec64dcc4a36b9e73602f
Dario Pompeu De Camargo Neto <dario.camargo@mercadolivre.com>
2026-01-02 19:47:04 -0300

    ping

--- src/main/java/com/br/fiap/entrypoinsts/controllers/HealthController.java
+++ src/main/java/com/br/fiap/entrypoinsts/controllers/HealthController.java
@@ -2,15 +2,17 @@ package com.br.fiap.entrypoinsts.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping
 public class HealthController {
 
-    @GetMapping("/ping")
+    @GetMapping({"/ping", "/ms-usuario/ping"})
     public ResponseEntity<Map<String, String>> ping() {
         Map<String, String> response = new HashMap<>();
         response.put("status", "ok");
