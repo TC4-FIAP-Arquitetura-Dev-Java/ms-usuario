8e7f6827a14da426e452fe4aeb0d4477487b6e13
Dario Pompeu De Camargo Neto <dario.camargo@mercadolivre.com>
2026-01-02 14:42:41 -0300

    mysql and /ping

---
+++ src/main/java/com/br/fiap/entrypoinsts/controllers/HealthController.java
@@ -0,0 +1,21 @@
+package com.br.fiap.entrypoinsts.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/ping")
+    public ResponseEntity<Map<String, String>> ping() {
+        Map<String, String> response = new HashMap<>();
+        response.put("status", "ok");
+        response.put("message", "pong");
+        return ResponseEntity.ok(response);
+    }
+}
+
