9d11b972cc121cb6a142cb1fa6c5ec422afea37d
Rodrigues <ghustavo516@gmail.com>
2026-01-07 23:05:33 -0300

    fix: Correção de testes

--- src/test/java/com/br/fiap/entrypoinsts/controllers/HealthControllerTest.java
+++ src/test/java/com/br/fiap/entrypoinsts/controllers/HealthControllerTest.java
@@ -1,18 +1,25 @@
 package com.br.fiap.entrypoinsts.controllers;
 
+import com.br.fiap.infraestrutura.config.security.JwtUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(HealthController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class HealthControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockitoBean
+    private JwtUtil jwtUtil;
 
     @Test
     void ping_shouldReturnOkStatusAndPongMessage() throws Exception {
